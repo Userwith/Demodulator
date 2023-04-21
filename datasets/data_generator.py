b8fbbe7a2c9f7ad442d60d810a1fb8d98382b128
chemora <314575057@qq.com>
2023-04-21 23:44:55 +0800

    2.0

--- datasets/data_generator.py
+++ datasets/data_generator.py
@@ -26,31 +26,52 @@ class Simulator:
         self.test_datasets_path = hps.simulator.test_datasets_path
         self.n_scale_max = hps.simulator.n_scale_max
         self.i_scale_max = hps.simulator.i_scale_max
-        self.i_scale_list = torch.arange(0,self.i_scale_max,self.num_i_scale)
-        self.n_scale_list = torch.arange(0,self.n_scale_max,self.num_n_scale)
-        self.data_type = {"train":self.train_datasets_path,"eval":self.eval_datasets_path,"test":self.test_datasets_path}
-
+        self.i_scale_list = torch.arange(0, self.i_scale_max, self.num_i_scale)
+        self.n_scale_list = torch.arange(0, self.n_scale_max, self.num_n_scale)
+        self.data_type = {"train": self.train_datasets_path, "eval": self.eval_datasets_path,
+                          "test": self.test_datasets_path}
+        self.n = torch.arange(self.t * self.fs).unsqueeze(0).repeat((self.batch_size, 1))
     def signal_wave(self):
-        Ts = 1 / self.fs
-        n = self.t / Ts
-        n = torch.arange(n).unsqueeze(0).repeat((self.batch_size, 1))
-        y = self.signal_amp * torch.sin(2 * torch.pi * self.signal_f * n * Ts + self.signal_phi * (torch.pi / 180))
+        y = self.signal_amp * torch.sin(2 * torch.pi * self.signal_f * self.n * (1/self.fs) + self.signal_phi * (torch.pi / 180))
         return y
 
     def inter_wave(self):
-        Ts = 1 / self.fs
-        n = self.t / Ts
-        n = torch.arange(n).unsqueeze(0).repeat((self.batch_size, 1))
-        y = self.inter_amp * torch.sin(2 * torch.pi * self.inter_f * n * Ts + self.inter_phi * (torch.pi / 180))
+        y = self.inter_amp * torch.sin(2 * torch.pi * self.inter_f * self.n * (1/self.fs) + self.inter_phi * (torch.pi / 180))
         return y
 
     def raw_wave(self, s_wave, i_wave, i_scales, n_scale):
         return s_wave + i_wave * i_scales + n_scale * torch.randn(s_wave.shape)
-    def _write(self,batch_id,i_scale,n_scale,data_type,data):
-        data_path = self.data_type[data_type]+"i_"+str(i_scale)+"_n_"+str(n_scale)
+
+    def _write(self, batch_id, i_scale, n_scale, data_type, s_wave, i_wave, r_wave):
+        data_path = self.data_type[data_type] + "i_" + str(i_scale) + "_n_" + str(n_scale)
         is_exists = os.path.exists(data_path)
         if not is_exists:
             os.makedirs(data_path.decode("utf-8"))
-        
+        torch.save(s_wave, "s_" + str(batch_id) + '.pth')
+        torch.save(i_wave, "i_" + str(batch_id) + '.pth')
+        torch.save(r_wave, "r_" + str(batch_id) + '.pth')
 
     def __call__(self, *args, **kwargs):
+        for i in self.i_scale_list:
+            for n in self.n_scale_list:
+                for batch in range(int(np.floor(self.num_train_data / self.batch_size))):
+                    s_wave = self.signal_wave()
+                    i_wave = self.inter_wave()
+                    i_scale = i * torch.rand(self.batch_size)
+                    n_scale = n
+                    r_wave = self.raw_wave(s_wave, i_wave, i_scale, n_scale)
+                    self._write(batch, i, n, "train", s_wave, i_wave, r_wave)
+                for batch in range(int(np.floor(self.num_eval_data / self.batch_size))):
+                    s_wave = self.signal_wave()
+                    i_wave = self.inter_wave()
+                    i_scale = i * torch.rand(self.batch_size)
+                    n_scale = n
+                    r_wave = self.raw_wave(s_wave, i_wave, i_scale, n_scale)
+                    self._write(batch, i, n, "eval", s_wave, i_wave, r_wave)
+                for batch in range(int(np.floor(self.num_test_data / self.batch_size))):
+                    s_wave = self.signal_wave()
+                    i_wave = self.inter_wave()
+                    i_scale = i * torch.rand(self.batch_size)
+                    n_scale = n
+                    r_wave = self.raw_wave(s_wave, i_wave, i_scale, n_scale)
+                    self._write(batch, i, n, "test", s_wave, i_wave, r_wave)
